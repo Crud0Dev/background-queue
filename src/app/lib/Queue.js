d110d02f6d0d8513a0e7c38b29bd5e3c5930c42e
Daniel Crudo <daniel.augusto.silva@accenture.com>
2021-04-24 15:43:26 -0300

    resolve problem lib

--- src/app/lib/Queue.js
+++ src/app/lib/Queue.js
@@ -7,23 +7,24 @@ const queues = Object.values(jobs).map(job => ({
   bull: new Queue(job.key, redisConfig),
   name: job.key,
   handle: job.handle,
+  options: job.options,
 }))
 
 export default {
   queues,
   add(name, data) {
-    const queue = this.queues.find(queue => queue.name === name); 
-
-    return queue.bull.add(data);
+    const queue = this.queues.find(queue => queue.name === name);
+    
+    return queue.bull.add(data, queue.options);
   },
   process() {
     return this.queues.forEach(queue => {
       queue.bull.process(queue.handle);
 
       queue.bull.on('failed', (job, err) => {
-           console.log('Job failed', queue.key, job.data)
-           console.log(err);
-         });
+        console.log('Job failed', queue.key, job.data);
+        console.log(err);
+      });
     })
   }
 };
--- src/server.js
+++ src/server.js
@@ -1,18 +1,17 @@
 import 'dotenv/config';
 import express from 'express';
-import userController from './app/controllers/userController';
-import Bullboard from 'bull-board';
+import UserController from './app/controllers/UserController';
+import BullBoard from 'bull-board';
 import Queue from './app/lib/Queue';
 
-
 const app = express();
-Bullboard.setQueues(Queue.queues.map(queue => queue.bull));
+BullBoard.setQueues(Queue.queues.map(queue => queue.bull));
 
 app.use(express.json());
-app.post('/users', userController.store);
+app.post('/users', UserController.store);
 
-app.use('/admin/queues', Bullboard.UI)
+app.use('/admin/queues', BullBoard.UI);
 
 app.listen(3333, () => {
-  console.log('Server running on port 3333')
+  console.log('Server running on localhost:3333');
 });
