730a2d9d52cb8ea82bc7699a282a99cc86fd774d
Daniel Crudo <daniel.augusto.silva@accenture.com>
2021-04-24 11:04:29 -0300

    commit master of the world

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import express from 'express';
+import userController from './app/controllers/userController';
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/users', userController.store);
+
+app.listen(3333, () => {
+  console.log('Server running on port 3333')
+});
